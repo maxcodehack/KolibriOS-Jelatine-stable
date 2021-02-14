d92355e6050b006e32715c7f932d431501c20135
maxcodehack <kuzmitskimaksim@gmail.com>
2021-02-14 17:04:53 +0300

    Fix

--- classpath/build.sh
+++ classpath/build.sh
@@ -1,4 +1,4 @@
 srcdir=src
 dstdir=bin
 mkdir bin
-/usr/java/j2sdk1.4.2_06/bin/javac -classpath $srcdir -bootclasspath $srcdir -d $dstdir -source 1.4 `find ./ -iname '*.java'` 
+/usr/java/jdk1.5.0/bin/javac -classpath $srcdir -bootclasspath $srcdir -d $dstdir -source 1.4 `find ./ -iname '*.java'` 
